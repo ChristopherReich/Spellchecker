59a2f59de841e75edd91943d4b58b82386b88a86
reich <reich@DESKTOP-1S33QIE>
2022-10-24 09:16:42 +0200

    Projektdateien hinzufügen.

---
+++ Spellchecker/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Spellchecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Spellchecker.myDictionary dictionary = new Spellchecker.myDictionary();
+           
+            Console.WriteLine("Bitte geben Sie einen Satz ein!");
+
+            string userInput = Console.ReadLine();
+            Regex rgx = new Regex("[^a-zA-Z -]");
+
+            string[] splittedInput = userInput.Split(' ');
+
+            foreach (var item in splittedInput)
+            {
+                string itemRgx = rgx.Replace(item, "");
+                if (dictionary.Contains(itemRgx))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(itemRgx + " ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(item + " ");
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Spellchecker/myDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellchecker
+{
+    public class myDictionary
+    {
+        libLinkedList.ILinkedList<string> myDict = new libLinkedList.SinglyLinkedList<string>();
+
+
+        public myDictionary()
+        {
+            this.Init();
+        }
+
+
+        internal void Init()
+        {
+            string source = "C:\\Users\\reich\\source\\repos\\Spellchecker\\german.dic";
+            string[] lines = System.IO.File.ReadAllLines(source);
+            foreach (string s in lines)
+            {
+                myDict.Add(s);
+            }
+            Console.WriteLine("Wörterbuch initialisiert!");
+        }
+
+        public bool Contains(string s)
+        {
+            return myDict.Contains(s);
+        }
+    }
+}
